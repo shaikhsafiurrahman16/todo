15d314a377e16e1b7bab96e6bc57d34f5f6885da
shaikhsafiurrahman16 <shaikhsafiurrahman16@gmail.com>
2025-09-03 21:15:58 +0500

    create completed

--- routes/crud.js
+++ routes/crud.js
@@ -1,10 +1,38 @@
 var express = require("express");
 var router = express.Router();
+const execute = require("../models/database/db");
+const { body, validationResult } = require("express-validator");
 
-router.post("/create", async (req, res) => {
-    res.send("create")
-});
-
-
+router.post(
+  "/create",
+  [
+    body("title").notEmpty().withMessage("Title is required").isString(),
+    body("duedate").notEmpty().withMessage("date is required"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors);
+      return res.json({ errors: errors });
+    } else {
+      const { title, description, duedate, color, priorty } = req.body;
+      console.log(req.body);
+      try {
+        await execute(
+          "INSERT INTO second (title, description, duedate, color, priorty) VALUES (?, ?, ?, ?, ?)",
+          [title, description, duedate, color, priorty]
+        );
+        res.json({ status: true, message: "Todo inserted" });
+      } catch (err) {
+        console.error(err);
+        res.json({
+          status: false,
+          message: "Insert failed",
+          error: err.message,
+        });
+      }
+    }
+  }
+);
 
 module.exports = router;
