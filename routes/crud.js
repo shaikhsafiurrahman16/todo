f00be1dafce0ade6f4cb0003bc39aed21025c3c8
shaikhsafiurrahman16 <shaikhsafiurrahman16@gmail.com>
2025-09-10 16:09:26 +0500

    todo by pages done

--- routes/crud.js
+++ routes/crud.js
@@ -8,20 +8,34 @@ router.post(
   "/create",
   authMiddleware,
   [
-    body("title").notEmpty().withMessage("Title is required").isString(),
-    body("duedate").notEmpty().withMessage("date is required"),
+    body("title")
+      .notEmpty()
+      .withMessage("Title is required")
+      .bail()
+      .matches(/^[A-Za-z0-9 ]+$/)
+      .withMessage("Title should only contain letters or numbers")
+      .trim()
+      .isLength({ min: 3 })
+      .withMessage("Title must be contain minimum 3 character"),
+    body("duedate")
+      .notEmpty()
+      .withMessage("date is required")
+      .isAfter(new Date().toISOString().split("s")[0])
+      .withMessage("Please enter a correct date"),
     body("color")
       .isIn(["red", "yellow", "green"])
       .withMessage("Select one of these red,yellow,green"),
     body("priorty")
-      .isIn(["first", "second", "third"])
-      .withMessage("Select one of these first,second,third"),
+      .isIn(["high", "medium", "low"])
+      .withMessage("Select one of these high , low , medium"),
   ],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      console.log(errors);
-      return res.json({ status: false, message: "Missing params" });
+      return res.json({
+        status: false,
+        message: errors.array().map((err) => err.msg),
+      });
     } else {
       const { title, description, duedate, color, priorty } = req.body;
       console.log(req.body);
@@ -42,88 +56,168 @@ router.post(
     }
   }
 );
-router.get("/read", authMiddleware, async (req, res) => {
-  try {
-    const rows = await execute("SELECT * FROM second WHERE user_id = ?", [
-      req.user.userId,
-    ]);
-    res.json({ status: true, message: "Data fetched", data: rows });
-  } catch (err) {
-    console.error(err);
-    res.json({
-      status: false,
-      message: "Data not fetched",
-      error: err.message,
-    });
-  }
-});
-router.delete("/delete", authMiddleware, async (req, res) => {
-  const { id } = req.body;
-
-  try {
-    const result = await execute(
-      "DELETE FROM second WHERE id = ? AND user_id = ?",
-      [id, req.user.userId]
-    );
-
-    if (result.affectedRows > 0) {
-      res.json({
-        status: true,
-        message: `Id ${id} deleted successfully`,
-      });
-    } else {
-      res.json({
-        status: false,
-        message: `Not allowed or Todo not found`,
-      });
-    }
-  } catch (err) {
-    console.error(err);
-    res.json({
-      status: false,
-      message: "Invalid",
-      error: err.message,
-    });
-  }
-});
-router.put(
-  "/update",
+router.get(
+  "/read",
   authMiddleware,
   [
-    body("title").notEmpty(),
-    body("duedate").notEmpty(),
-    body("color").isIn(["red", "yellow", "green"]),
-    body("priorty").isIn(["first", "second", "third"]),
+    body("page").isInt().withMessage("please insert a number"),
+    body("limit").isInt().withMessage("please insert a number"),
   ],
   async (req, res) => {
-    const { id, title, description, duedate, color, priorty } = req.body;
-    if (!id) {
-      return res.json({ status: false, message: "Todo ID is required" });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({
+        status: false,
+        message: errors.array().map((err) => err.msg),
+      });
     } else {
       try {
-        const result = await execute(
-          `UPDATE second 
-       SET title = ?, description = ?, duedate = ?, color = ?, priorty = ?, updatedAt = NOW() 
-       WHERE id = ? AND user_id = ?`,
-          [title, description, duedate, color, priorty, id, req.user.userId]
+        const page = parseInt(req.body.page || 1);
+        const limit = parseInt(req.body.limit || 10);
+        const offset = (page - 1) * limit;
+
+        const totalRows = await execute(
+          "SELECT COUNT(*) AS total FROM second WHERE user_id = ?",
+          [req.user.userId]
+        );
+        const totalTodos = totalRows[0].total;
+
+        const rows = await execute(
+          "SELECT * FROM second WHERE user_id = ? LIMIT ? OFFSET ?",
+          [req.user.userId, limit, offset]
         );
 
-        if (result.affectedRows > 0) {
-          res.json({ status: true, message: `Id ${id} updated successfully` });
-        } else {
-          res.json({
-            status: false,
-            message: `Not allowed or Todo not found`,
-          });
-        }
+        res.json({
+          status: true,
+          message: "Data fetched",
+          currentPage: page,
+          totalPages: Math.ceil(totalTodos / limit),
+          totalTodos,
+          data: rows,
+        });
       } catch (err) {
         console.error(err);
         res.json({
           status: false,
-          message: "Update failed",
-          error: err.message,
+          message: "Data not fetched",
         });
       }
+    }
+  }
+);
+router.delete(
+  "/delete",
+  authMiddleware,
+  [
+    body("id")
+      .notEmpty()
+      .withMessage("Id is required")
+      .bail()
+      .isInt()
+      .withMessage("Id must be contains a number"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({
+        status: false,
+        message: errors.array().map((err) => err.msg),
+      });
+    }
+    const { id } = req.body;
+
+    try {
+      const result = await execute(
+        "DELETE FROM second WHERE id = ? AND user_id = ?",
+        [id, req.user.userId]
+      );
+
+      if (result.affectedRows > 0) {
+        res.json({
+          status: true,
+          message: `Id ${id} deleted successfully`,
+        });
+      } else {
+        res.json({
+          status: false,
+          message: `Not allowed or Todo not found`,
+        });
+      }
+    } catch (err) {
+      console.error(err);
+      res.json({
+        status: false,
+        message: "Invalid",
+        error: err.message,
+      });
+    }
+  }
+);
+router.put(
+  "/update",
+  authMiddleware,
+  [
+    body("id")
+      .notEmpty()
+      .withMessage("Id is required")
+      .bail()
+      .isInt()
+      .withMessage("Id must be contains a number"),
+    body("title")
+      .notEmpty()
+      .withMessage("Title is required")
+      .bail()
+      .matches(/^[A-Za-z0-9 ]+$/)
+      .withMessage("Title should only contain letters or numbers")
+      .bail()
+      .isLength({ min: 3 })
+      .withMessage("Title must be contain minimum 3 character"),
+    body("duedate")
+      .notEmpty()
+      .withMessage("date is required")
+      .bail()
+      .isAfter(new Date().toISOString().split("T")[0])
+      .withMessage("Please enter a correct date"),
+    body("color")
+      .isIn(["red", "yellow", "green"])
+      .withMessage("Select one of these red,yellow,green")
+      .bail(),
+    body("priorty")
+      .isIn(["high", "medium", "low"])
+      .withMessage("Select one of these high , low , medium"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({
+        status: false,
+        message: errors.array().map((err) => err.msg),
+      });
+    }
+    const { id, title, description, duedate, color, priorty } = req.body;
+    try {
+      const result = await execute(
+        `UPDATE second 
+       SET title = ?, description = ?, duedate = ?, color = ?, priorty = ?, updatedAt = NOW() 
+       WHERE id = ? AND user_id = ?`,
+        [title, description, duedate, color, priorty, id, req.user.userId]
+      );
+
+      if (result.affectedRows > 0) {
+        res.json({ status: true, message: `Id ${id} updated successfully` });
+      } else {
+        res.json({
+          status: false,
+          message: `Not allowed or Todo not found`,
+        });
+      }
+    } catch (err) {
+      console.error(err);
+      res.json({
+        status: false,
+        message: "Update failed",
+        error: err.message,
+      });
     }
   }
 );
